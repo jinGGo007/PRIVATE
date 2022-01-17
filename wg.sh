58c8baf7ce250d5e3b4a0dc6fbdc61f7b3f8aa6e
jinGGo007 <73293452+jinGGo007@users.noreply.github.com>
2022-01-17 14:41:27 +0800

    Update wg.sh

--- wg.sh
+++ wg.sh
@@ -88,11 +88,11 @@ WG_RUNNING=$?
 
 # Tambahan
 cd /usr/bin
-wget -O mwg "https://raw.githubusercontent.com/jinGGo007/SCRIPT/main/mwg.sh"
-wget -O add-wg "https://raw.githubusercontent.com/jinGGo007/SCRIPT/main/add-wg.sh"
-wget -O del-wg "https://raw.githubusercontent.com/jinGGo007/SCRIPT/main/del-wg.sh"
-wget -O cek-wg "https://raw.githubusercontent.com/jinGGo007/SCRIPT/main/cek-wg.sh"
-wget -O renew-wg "https://raw.githubusercontent.com/jinGGo007/SCRIPT/main/renew-wg.sh"
+wget -O mwg "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/mwg.sh"
+wget -O add-wg "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/add-wg.sh"
+wget -O del-wg "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/del-wg.sh"
+wget -O cek-wg "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/cek-wg.sh"
+wget -O renew-wg "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/renew-wg.sh"
 chmod +x mwg
 chmod +x add-wg
 chmod +x del-wg

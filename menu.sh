787a28594faf0b220d98ddf6cb1598aa17dc2b75
jinGGo007 <73293452+jinGGo007@users.noreply.github.com>
2022-01-17 14:13:39 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -14,32 +14,7 @@ clear
 MYIP=$(wget -qO- ifconfig.co);
 echo "Checking VPS"
 clear
-echo -e " "
 
-echo -e " "
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-CITY=$(curl -s ipinfo.io/city )
-WKT=$(curl -s ipinfo.io/timezone )
-IPVPS=$(curl -s ipinfo.io/ip )
-DOMAIN=$(cat /etc/v2ray/domain)
-	cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
-	cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
-	freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
-	tram=$( free -m | awk 'NR==2 {print $2}' )
-	swap=$( free -m | awk 'NR==4 {print $2}' )
-	up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
-
-
-echo -e  " $bl \e[032;1mCPU Model:\e[0m$bd $cname  "
-echo -e  " $bl \e[032;1mNumber Of Cores:\e[0m$bd $cores"
-echo -e  " $bl \e[032;1mCPU Frequency:\e[0m$bd $freq MHz"
-echo -e  " $bl \e[032;1mTotal Amount Of RAM:\e[0m$bd $tram MB"
-echo -e  " $op \e[032;1mSystem Uptime:\e[0m$bd $up"
-echo -e  " $op \e[032;1mIsp Name:\e[0m$bd $ISP"
-echo -e  " $op \e[032;1mIp Vps:\e[0m$bd $IPVPS"
-echo -e  " $op \e[032;1mCity:\e[0m$bd $CITY"
-echo -e  " $op \e[032;1mTime:\e[0m$bd $WKT "
-echo -e  " $op \e[032;1mDOMAIN:\e[0m $DOMAIN"
 echo -e  " AUTOSCRIPT LITE VERSION MODDED BY JINGGO007" | lolcat
 echo -e  " "
 echo -e  " ═════════════════════════════════════════════════════════════════ "

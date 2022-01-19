ae76606f5cf8fe21e4dcd626a1dcbee0aebaef6b
jinGGo007 <73293452+jinGGo007@users.noreply.github.com>
2022-01-19 21:27:42 +0800

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -189,33 +189,6 @@ wget https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/vpn.sh && chmod +x
 # install fail2ban
 apt -y install fail2ban
 
-# Install DDOS Flate
-if [ -d '/usr/local/ddos' ]; then
-	echo; echo; echo "Please un-install the previous version first"
-	exit 0
-else
-	mkdir /usr/local/ddos
-fi
-clear
-echo; echo 'Installing DOS-Deflate 0.6'; echo
-echo; echo -n 'Downloading source files...'
-wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
-echo -n '.'
-wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
-echo -n '.'
-wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
-echo -n '.'
-wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
-chmod 0755 /usr/local/ddos/ddos.sh
-cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
-echo '...done'
-echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
-/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
-echo '.....done'
-echo; echo 'Installation has completed.'
-echo 'Config file is at /usr/local/ddos/ddos.conf'
-echo 'Please send in your comments and/or suggestions to johnlabu2801@gmail.com'
-
 # banner /etc/issue.net
 wget -O /etc/issue.net "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/issue.net"
 echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config

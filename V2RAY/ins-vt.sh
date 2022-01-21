531b7468b3bdd07f3f00675a2d9ac422c3d37614
jinGGo007 <73293452+jinGGo007@users.noreply.github.com>
2022-01-22 02:36:55 +0800

    Update ins-vt.sh

--- V2RAY/ins-vt.sh
+++ V2RAY/ins-vt.sh
@@ -8,7 +8,7 @@ apt -y install chrony
 timedatectl set-ntp true
 systemctl enable chronyd && systemctl restart chronyd
 systemctl enable chrony && systemctl restart chrony
-timedatectl set-timezone Asia/Singapore
+timedatectl set-timezone Asia/Kuala_Lumpur
 chronyc sourcestats -v
 chronyc tracking -v
 date
@@ -16,7 +16,7 @@ date
 mkdir -p /etc/trojan/
 touch /etc/trojan/akun.conf
 # install v2ray
-wget https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/go.sh && chmod +x go.sh && ./go.sh
+wget https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/go.sh && chmod +x go.sh && ./go.sh
 rm -f /root/go.sh
 bash -c "$(wget -O- https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
 mkdir /root/.acme.sh
@@ -493,23 +493,23 @@ systemctl enable v2ray
 
 cd /usr/bin
 
-wget -O mssh "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/mssh.sh"
-wget -O mtrojan "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/mtrojan.sh"
-wget -O mvless "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/mvless.sh"
-wget -O mvmess "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/mvmess.sh"
-wget -O add-ws "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/add-ws.sh"
-wget -O add-vless "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/add-vless.sh"
-wget -O add-tr "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/add-tr.sh"
-wget -O del-ws "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/del-ws.sh"
-wget -O del-vless "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/del-vless.sh"
-wget -O del-tr "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/del-tr.sh"
-wget -O cek-ws "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/cek-ws.sh"
-wget -O cek-vless "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/cek-vless.sh"
-wget -O cek-tr "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/cek-tr.sh"
-wget -O renew-ws "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/renew-ws.sh"
-wget -O renew-vless "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/renew-vless.sh"
-wget -O renew-tr "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/renew-tr.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/cert.sh"
+wget -O mssh "https://raw.githubusercontent.com/jinGGo007/PRIVATE/SSHOVPN/main/mssh.sh"
+wget -O mtrojan "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/mtrojan.sh"
+wget -O mvless "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/mvless.sh"
+wget -O mvmess "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/mvmess.sh"
+wget -O add-ws "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/add-ws.sh"
+wget -O add-vless "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/add-vless.sh"
+wget -O add-tr "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/add-tr.sh"
+wget -O del-ws "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/del-ws.sh"
+wget -O del-vless "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/del-vless.sh"
+wget -O del-tr "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/del-tr.sh"
+wget -O cek-ws "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/cek-ws.sh"
+wget -O cek-vless "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/cek-vless.sh"
+wget -O cek-tr "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/cek-tr.sh"
+wget -O renew-ws "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/renew-ws.sh"
+wget -O renew-vless "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/renew-vless.sh"
+wget -O renew-tr "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/renew-tr.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/jinGGo007/PRIVATE/V2RAY/main/cert.sh"
 chmod +x mssh
 chmod +x mtrojan
 chmod +x mvless

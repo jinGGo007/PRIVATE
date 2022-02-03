490e9cd6419f095a9438f35bb6ed6fa3abbe8b28
jinGGo007 <73293452+jinGGo007@users.noreply.github.com>
2022-02-04 01:45:47 +0800

    Add files via upload

---
+++ V4/setup.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking Vps"
+sleep 2
+clear
+
+#Welcome Note
+echo -e "============================================="
+echo -e " ${green} WELCOME TO JINGGO SCRIPT${NC}"
+echo -e "============================================="
+sleep 2
+
+#Install Update
+echo -e "============================================="
+echo -e " ${green} UPDATE && UPGRADE PROCESS${NC}"
+echo -e "============================================="
+apt -y update 
+apt install -y bzip2 gzip coreutils screen curl
+sleep 2
+clear
+
+# Disable IPv6
+echo -e "============================================="
+echo -e " ${green} DISABLE IPV6${NC}"
+echo -e "============================================="
+sleep 2
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+echo -e "net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+sleep 2
+clear
+
+# Subdomain Settings
+echo -e "============================================="
+echo -e "${green} DOMAIN INPUT${NC} "
+echo -e "============================================="
+sleep 2
+mkdir /etc/v2ray
+mkdir /etc/xray
+mkdir /var/lib/premium-script;
+mkdir /var/lib/crot-script;
+clear
+echo -e ""
+echo -e "${green}MASUKKAN DOMAIN ANDA YANG TELAH DI POINT KE IP ANDA${NC}"
+read -rp "    Enter your Domain/Host: " -e host
+ip=$(wget -qO- ipv4.icanhazip.com)
+host_ip=$(ping "${host}" -c 1 | sed '1{s/[^(]*(//;s/).*//;q}')
+if [[ ${host_ip} == "${ip}" ]]; then
+	echo -e ""
+	echo -e "${green}HOST/DOMAIN MATCHED..INSTALLATION WILL CONTINUE${NC}"
+	echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
+	echo "IP=$host" >> /var/lib/crot-script/ipvps.conf
+    echo "$host" >> /etc/v2ray/domain
+    echo "$host" > /root/domain
+	sleep 2
+	clear
+else
+	echo -e "${green}HOST/DOMAIN NOT MATCHED..INSTALLATION WILL TERMINATED${NC}"
+	echo -e ""
+    rm -f setup.sh
+    exit 1
+fi
+sleep 1
+
+
+#install ssh ovpn
+echo -e "============================================="
+echo -e " ${green} INSTALLING SSH && OVPN && WS ${NC}"
+echo -e "============================================="
+sleep 2
+wget https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/V4/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+sleep 2
+clear
+
+#install v2ray
+echo -e "============================================="
+echo -e " ${green} INSTALLING V2RAY${NC} "
+echo -e "============================================="
+sleep 2
+wget https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/V2RAY/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+sleep 2
+clear
+
+#install Xray
+echo -e "============================================="
+echo -e " ${green} INSTALLING XRAY${NC} "
+echo -e "============================================="
+sleep 2
+wget https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/XRAY/install-xray.sh && chmod +x install-xray.sh && screen -S xray ./install-xray.sh
+sleep 2
+clear
+
+#install ohp
+echo -e "============================================="
+echo -e " ${green} INSTALLING OHP${NC} "
+echo -e "============================================="
+sleep 2
+wget https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/OHP/ohpserver.sh && chmod +x ohpserver.sh && ./ohpserver.sh
+sleep 2
+clear
+
+rm -f /root/ssh-vpn.sh
+rm -f /root/ins-vt.sh
+rm -f /root/ohpserver.sh
+
+apt install -y dos2unix && dos2unix running
+cat <<EOF> /etc/systemd/system/autosett.service
+[Unit]
+Description=autosetting
+Documentation=JINGGO007
+[Service]
+Type=oneshot
+ExecStart=/bin/bash /etc/set.sh
+RemainAfterExit=yes
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable autosett
+wget -O /etc/set.sh "https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/set.sh"
+chmod +x /etc/set.sh
+history -c
+echo "1.2" > /home/ver
+clear
+echo " "
+echo "INSTALLATION COMPLETE!!"
+echo " "
+echo "=================================-Autoscript Premium-===========================" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
+echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
+echo "   - Stunnel4                : 443, 777"  | tee -a log-install.txt
+echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - Nginx                   : 81"  | tee -a log-install.txt
+echo "   - SSH WS/OVPN WS          : 2082, 2095"  | tee -a log-install.txt
+echo "   - DROPBEAR OHP            : 8090"  | tee -a log-install.txt
+echo "   - OPENVPN OHP             : 8089"  | tee -a log-install.txt
+echo "   - V2RAY Vmess TLS         : 8443"  | tee -a log-install.txt
+echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
+echo "   - V2RAY Vless TLS         : 2083"  | tee -a log-install.txt
+echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
+echo "   - XRAY VLESS XTLS         : 6060"  | tee -a log-install.txt
+echo "   - XRAY VLESS SPLICE       : 6060"  | tee -a log-install.txt
+echo "   - XRAY VLESS WS XTLS      : 6060"  | tee -a log-install.txt
+echo "   - XRAY VLESS None TCP     : 6666"  | tee -a log-install.txt
+echo "   - XRAY VLESS TCP          : 6565"  | tee -a log-install.txt
+echo "   - XRAY VMESS NONE TCP     : 6464"  | tee -a log-install.txt
+echo "   - XRAY VMESS TCP          : 6363"  | tee -a log-install.txt
+echo "   - Trojan                  : 2087"  | tee -a log-install.txt
+echo "   - Trojan GO               : 2096"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/MALAYSIA (GMT +8)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 GMT +8" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   - Dev/Main                : Horas"  | tee -a log-install.txt
+echo "   - Modded by               : JINGGO007"  | tee -a log-install.txt
+echo "   - Telegram                : t.me/jinggo007"  | tee -a log-install.txt
+echo "   - Instagram               : None"  | tee -a log-install.txt
+echo "   - Whatsapp                : 999"  | tee -a log-install.txt
+echo "   - Facebook                : johnlabu" | tee -a log-install.txt
+echo "------------------Script Modded By JINGGO007-----------------" | tee -a log-install.txt
+echo ""
+sleep 1
+rm -f setup.sh
+read -n 1 -r -s -p $'Press any key to reboot...\n';reboot

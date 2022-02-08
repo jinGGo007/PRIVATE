6cc119179ac0b5af4dcc75be8b7be1aa2ca88ad8
jinGGo007 <73293452+jinGGo007@users.noreply.github.com>
2022-02-09 01:29:39 +0800

    Add files via upload

--- XRAY/port-xtrgo.sh
+++ XRAY/port-xtrgo.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
-MYIP=$(wget -qO- icanhazip.com);
-echo "Checking Vps"
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ifconfig.me/ip);
+echo "Checking VPS"
 clear
-tr="$(cat ~/log-install.txt | grep -i "TROGAN GO" | cut -d: -f2|sed 's/ //g')"
+tr="$(cat ~/log-install.txt | grep -w "TROGAN GO" | cut -d: -f2|sed 's/ //g')"
 echo -e "Name : Change Port Trojan GO"
-echo -e "============================" | lolcat
+echo -e "============================" 
 echo -e "Change Port $tr"
 read -p "New Port Trojan-go: " tr2
 if [ -z $tr2 ]; then
 echo "Please Input Port"
 exit 0
 fi
-cek=$(netstat -nutlp | grep -w $tr2)
+cek=$(netstat -nutlp | grep -w $tls1)
 if [[ -z $cek ]]; then
-sed -i "s/$tr/$tr2/g" /etc/xray/trojan.json
-sed -i "s/   - TROGAN GO                  : $tr/   - TROGAN GO                  : $tr2/g" /root/log-install.txt
-iptables -D INPUT -m state --state NEW -m tcp -p tcp --dport $tr -j ACCEPT
-iptables -D INPUT -m state --state NEW -m udp -p udp --dport $tr -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport $tr2 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport $tr2 -j ACCEPT
+sed -i "s/$tls/$tls1/g" /etc/xray/trojan.json
+sed -i "s/   - TROGAN GO          : $tls/   - TROGAN GO          : $tls1/g" /root/log-install.txt
+iptables -D INPUT -m state --state NEW -m tcp -p tcp --dport $tls -j ACCEPT
+iptables -D INPUT -m state --state NEW -m udp -p udp --dport $tls -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport $tls1 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport $tls1 -j ACCEPT
 iptables-save > /etc/iptables.up.rules
 iptables-restore -t < /etc/iptables.up.rules
 netfilter-persistent save > /dev/null
 netfilter-persistent reload > /dev/null
+systemctl stop x-tr.service > /dev/null
+systemctl enable x-tr.service > /dev/null
+systemctl start x-tr.service > /dev/null
 systemctl restart x-tr.service > /dev/null
 echo -e "\e[032;1mPort $tr2 modified successfully\e[0m"
 else

407e51f972bc42942a425edd9682579e1d4e61ac
jinGGo007 <73293452+jinGGo007@users.noreply.github.com>
2022-02-21 19:50:23 +0800

    Update ssh-vpn.sh

--- V3/ssh-vpn.sh
+++ V3/ssh-vpn.sh
@@ -76,7 +76,7 @@ ln -sf /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
 sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 
 # install
-apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
+apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl git lsof
 echo "clear" >> .profile
 echo "jinggo" >> .profile
 
